de3d53126e7310b4c7dfac629bff8c82902edb05
quangBig <ledai16102004@gmail.com>
2025-09-22 02:45:09 +0700

    update: PORT

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Nếu có PORT trong env thì dùng, không thì fallback về 3000
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  console.log(`🚀 Server is running on http://localhost:${port}`);
 }
 bootstrap();
